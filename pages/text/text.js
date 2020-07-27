3791f5ec69e9b43809793036140ddb79ce9d1420
pixinlei <2954716615@qq.com>
2020-07-27 23:01:46 +0800

    a

--- pages/text/text.js
+++ pages/text/text.js
@@ -1,3 +1,9 @@
+var touchDot = 0;//触摸时的原点
+
+var time = 0;// 时间记录，用于滑动时且时间小于1s则执行左右滑动
+
+var interval = "";// 记录/清理时间记录
+
 Page({
 
   /**
@@ -22,5 +28,70 @@ Page({
       fail: function(res) {},
       complete: function(res) {},
     })
+  },
+
+
+// 触摸开始事件
+
+touchStart: function (e) {
+
+  touchDot = e.touches[0].pageX; // 获取触摸时的原点
+
+  // 使用js计时器记录时间 
+
+  interval = setInterval(function () {
+
+   time++;
+
+  }, 100);
+
+ },
+
+ // 触摸移动事件
+
+ touchMove: function (e) {
+
+  var touchMove = e.touches[0].pageX;
+
+  console.log("touchMove:" + touchMove + " touchDot:" + touchDot + " diff:" + (touchMove - touchDot));
+
+  // 向左滑动 
+
+  if (touchMove - touchDot <= -40 && time < 10) {
+
+   wx.switchTab({
+
+    url: '../text2/text2'
+
+   }); 
+
   }
+
+  // 向右滑动
+
+  if (touchMove - touchDot >= 40 && time < 10) {
+
+   console.log('向右滑动');
+
+   wx.switchTab({
+
+    url: "../text2/text2"
+
+   }); 
+
+  }
+
+ },
+
+ // 触摸结束事件
+
+ touchEnd: function (e) {
+
+  clearInterval(interval); // 清除setInterval
+
+  time = 0;
+
+ },
+
+
 })
