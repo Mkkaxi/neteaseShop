27d0d68b5d8364354b10785c484c9eb963438ff3
pixinlei <2954716615@qq.com>
2020-07-28 21:09:38 +0800

    7-28

--- pages/sort/sort.js
+++ pages/sort/sort.js
@@ -1,4 +1,9 @@
 // pages/sort/sort.js
+var touchDot = 0;//触摸时的原点
+
+var time = 0;// 时间记录，用于滑动时且时间小于1s则执行左右滑动
+
+var interval = "";// 记录/清理时间记录
 Page({
 
   /**
@@ -130,7 +135,67 @@ Page({
               }
             ]
   },
+// 触摸开始事件
 
+touchStart: function (e) {
+
+  touchDot = e.touches[0].pageX; // 获取触摸时的原点
+
+  // 使用js计时器记录时间 
+
+  interval = setInterval(function () {
+
+   time++;
+
+  }, 100);
+
+ },
+
+ // 触摸移动事件
+
+ touchMove: function (e) {
+
+  var touchMove = e.touches[0].pageX;
+
+  console.log("touchMove:" + touchMove + " touchDot:" + touchDot + " diff:" + (touchMove - touchDot));
+
+  // 向左滑动 
+
+  if (touchMove - touchDot <= -40 && time < 10) {
+
+   wx.switchTab({
+
+    url: '../worthbuy/worthbuy'
+
+   }); 
+
+  }
+
+  // 向右滑动
+
+  if (touchMove - touchDot >= 40 && time < 10) {
+
+   console.log('向右滑动');
+
+   wx.switchTab({
+
+    url: "../worthbuy/worthbuy"
+
+   }); 
+
+  }
+
+ },
+
+ // 触摸结束事件
+
+ touchEnd: function (e) {
+
+  clearInterval(interval); // 清除setInterval
+
+  time = 0;
+
+ },
 
   /**
    * 生命周期函数--监听页面加载
