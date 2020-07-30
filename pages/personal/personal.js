14c3c4732753bbabf0fce47f7f1870fbfac67e6c
pixinlei <2954716615@qq.com>
2020-07-30 22:13:14 +0800

    a

---
+++ pages/personal/personal.js
@@ -0,0 +1,85 @@
+// pages/personal/personal.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    myitems:[
+      {price:"￥0",name:'回馈金'},
+      {price:"7",name:'红包'},
+      {price:"1",name:'优惠券'},
+      {price:"0",name:'津贴'},
+      {price:"0",name:'礼品卡'},
+    ],
+    myserve:[
+      {pic:'../../images/订单.png',title:'我的订单'},
+      {pic:'../../images/周六.png',title:'周六一起拼'},
+      {pic:'../../images/售后.png',title:'售后服务'},
+      {pic:'../../images/邀请.png',title:'邀请返利'},
+      {pic:'../../images/优先购.png',title:'优先购'},
+      {pic:'../../images/积分中心.png',title:'积分中心'},
+      {pic:'../../images/会员俱乐部.png',title:'会员俱乐部'},
+      {pic:'../../images/地址管理.png',title:'地址管理'},
+      {pic:'../../images/账号安全.png',title:'帐号安全'},
+      {pic:'../../images/帮助与客服.png',title:'帮助与客服'},
+      {pic:'../../images/意见反馈.png',title:'意见反馈'},
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
